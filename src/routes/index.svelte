4e8e8e9bdd8765bbabf1d24a235b1c834c564632
Matt Pilott <matt@creativelittledots.co.uk>
2022-01-20 12:50:41 +0000

    Initial

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+   export async function load({ fetch, url }) {
+      const res = await fetch(`/api.json`)
+
+      if ( res.ok ) return { props: await res.json() }
+   }
+</script>
+
+<script>
+   export let success = false
+</script>
+{success}
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
